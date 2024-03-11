70cabfd63dccc69fda7e5b05324ddd437c616b55
Ríad Oliveira <riad.oliveira@hotmail.com>
2024-03-11 10:38:15 -0300

    AccountService finished

--- src/components/AccountService.java
+++ src/components/AccountService.java
@@ -9,4 +9,49 @@ import interfaces.IAccountService;
 import utils.PasswordHasher;
 
 public class AccountService implements IAccountService {
+  private final List<User> accountsDatabase = new ArrayList<>();
+
+  @Override
+  public void createAccount(User user) throws Exception {
+    boolean existsByEmail = findByEmail(user.getEmail()) != null;
+    if(existsByEmail) {
+      throw new AppException(
+        "Um usuário com esse e-mail já existe!"
+      );
+    }
+
+    user.setPassword(
+      PasswordHasher.hashAndEncode(user.getPassword())
+    );
+    accountsDatabase.add(user);
+  }
+
+  @Override
+  public User authenticate(
+    String email, String password
+  ) throws Exception {
+    User findedUser = findByEmail(email);
+    if(findedUser == null) {
+      throw new AppException(
+        "Usuário requisitado não existe!"
+      );
+    }
+
+    boolean validPassword = PasswordHasher.passwordsAreEqual(
+      findedUser.getPassword(), password
+    );
+    if(!validPassword) {
+      throw new AppException("Email ou senha inválido(s)!");
+    }
+
+    return findedUser;
+  }
+
+  private User findByEmail(String email) {
+    for(User account : accountsDatabase) {
+      if(account.getEmail().equals(email)) return account;
+    }
+
+    return null;
+  }
 }
--- src/interfaces/IAccountService.java
+++ src/interfaces/IAccountService.java
@@ -2,5 +2,9 @@ package interfaces;
 
 import java.rmi.Remote;
 
+import dtos.user.User;
+
 public interface IAccountService extends Remote {
+  public void createAccount(User user) throws Exception;
+  public User authenticate(String email, String password) throws Exception;
 }
